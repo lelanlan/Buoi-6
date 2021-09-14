d2f9a1e4b76d57df5ecd876c142c8cdbcbd04b41
lelanlan <90110488+lelanlan@users.noreply.github.com>
2021-09-14 15:32:00 +0700

    Update Lat_chuoi.py

--- Lat_chuoi.py
+++ Lat_chuoi.py
@@ -1,6 +1,17 @@
+s=""
 str="tHE fOX iS cOMING fOR tHE cHICKEN"
 l=str.split(" ")
 n= len(l)
 for i in range(0,n):
-    s = s + l[n-i-1].lower() + " "
-print(s)
+    s = s + l[n-i-1] + " "
+s1 = ""
+
+for i in s:
+    if i.isupper():
+        i=i.lower()
+    else:
+        i=i.upper()
+
+    s1 = s1 + i + " "
+
+print(s1)
